83de4cb52f1ee4f5aeb1e93cc79f686cf6f33286
kathrynzaro <zarokathryn@gmail.com>
2022-08-23 11:57:13 -0700

    /me route test passes

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -10,8 +10,26 @@ const mockUser = {
   password: 'ilovezero',
 };
 
-describe('user routes', () => {
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
 
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  // const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  // const { email } = user;
+  const resp = await agent
+    .post('/api/v1/users')
+    .send({ ...mockUser, ...userProps });
+  const user = resp.body;
+  return [agent, user];
+};
+
+describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -22,7 +40,7 @@ describe('user routes', () => {
   it('#POST /api/v1/users/ creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     const { firstName, lastName, email } = mockUser;
-    
+
     expect(res.body).toEqual({
       id: expect.any(String),
       firstName,
@@ -31,4 +49,19 @@ describe('user routes', () => {
     });
   });
 
+  it('GET /me should return the currently logged in user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const resp = await agent.get('/api/v1/users/me');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
+  });
+
+  it('GET /me should return a 401 if not logged in', async () => {
+    const resp = await request(app).get('/api/v1/users/me');
+    expect(resp.status).toBe(401);
+  });
 });
