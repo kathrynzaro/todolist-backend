d1fd5f0679a12d33ad3af466e722b2ecd36fc660
kathrynzaro <zarokathryn@gmail.com>
2022-08-23 15:10:06 -0700

    item post route

---
+++ __tests__/tasks.test.js
@@ -0,0 +1,51 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+const Task = require('../lib/models/Task');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('items', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('#POST /api/v1/task creates a new task', async () => {
+    const [agent, user] = await registerAndLogin();
+    const task = { description: 'vacuum' };
+    const res = await agent.post('/api/v1/tasks').send(task);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      description: task.description,
+      user_id: user.id,
+      complete: false,
+    });
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
+DROP TABLE IF EXISTS todo_tasks;
 DROP TABLE IF EXISTS todo_users;
 
 CREATE TABLE todo_users (
@@ -10,3 +11,13 @@ CREATE TABLE todo_users (
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
 );
+
+CREATE TABLE todo_tasks (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  description VARCHAR NOT NULL,
+  complete BOOLEAN NOT NULL DEFAULT(false),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES todo_users(id)
+);
+
